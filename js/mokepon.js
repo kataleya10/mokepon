3419ff3b9b01cf515f73abc3a606399aa0c77fab
kataleya10 <136022898+kataleya10@users.noreply.github.com>
2023-08-20 11:47:43 +0200

    added event listener

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,1 +1,10 @@
-alert("hola mundp js")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", selecionarMascotaJugador)
+}
+
+function selecionarMascotaJugador() {
+    alert("seleccionaste tu mascota")
+}
+
+window.addEventListener("load", iniciarJuego)
