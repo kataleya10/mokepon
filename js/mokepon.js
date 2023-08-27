ed42708a9cc1e7dd0db046df8e62d7e14dcd754f
kataleya10 <136022898+kataleya10@users.noreply.github.com>
2023-08-27 17:39:27 +0200

    added aleatory

--- js/mokepon.js
+++ js/mokepon.js
@@ -9,16 +9,33 @@ function selecionarMascotaJugador() {
     let inputCapipepo = document.getElementById('Capipepo')
     let inputRatigueya = document.getElementById('Ratigueya')
     let spanMascotaJugador = document.getElementById('mascota-jugador')
-    
+
     if (inputHipodoge.checked) {
-        spanMascotaJugador.innerHTML= "Hipodogue"
+        spanMascotaJugador.innerHTML = "Hipodogue"
     } else if (inputCapipepo.checked) {
-        spanMascotaJugador.innerHTML= "Capipepo"
+        spanMascotaJugador.innerHTML = "Capipepo"
     } else if (inputRatigueya.checked) {
-        spanMascotaJugador.innerHTML= "Ratigueya"
+        spanMascotaJugador.innerHTML = "Ratigueya"
     } else {
         alert('Selecciona una mascota 😒')
     }
+    SeleccionarMascotaEnemigo()
+}
+function SeleccionarMascotaEnemigo() {
+let ataqueAleatorio= aleatorio(1,3)
+let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 window.addEventListener("load", iniciarJuego)
